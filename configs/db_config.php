e2bde69340f501eebdadf48f6c54f815c0567003
Afrid2004 <mdfaisalafrid@gmail.com>
2026-07-15 12:40:48 +0600

    added databases

--- configs/db_config.php
+++ configs/db_config.php
@@ -1,17 +1,16 @@
 <?php   
    //Remote
    
+    //  define("SERVER","localhost");
+    //  define("USER","faisalfr_fast_drop_2026");
+    //  define("DATABASE","faisalfr_fast_drop_2026");
+    //  define("PASSWORD","fast_drop_2026");
+
+   //local
      define("SERVER","localhost");
      define("USER","root");
      define("DATABASE","fast_drop_2026");
      define("PASSWORD","");
-
-   //Local
-   
-    //define("SERVER","localhost");
-    //define("USER","root");//rajib
-    //define("DATABASE","hosting");
-    //define("PASSWORD","");
 
     $db=new mysqli(SERVER,USER,PASSWORD,DATABASE);
     $tx="";
